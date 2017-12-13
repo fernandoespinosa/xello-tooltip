dfadb640403c80e55a08e2ba576d5539e26adcb1
fernandoespinosa@fernandoespinosa.org <fernandoespinosa@fernandoespinosa.org>
2017-12-12 22:35:26 -0800

    [mod] tooltip-manager.directive:
    	- selector: '[tooltipManager][ngbTooltip]'
    	- constructor: log injected objects

--- src/app/directives/tooltip-manager.directive.ts
+++ src/app/directives/tooltip-manager.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap/tooltip/tooltip';
 
 @Directive({
-  selector: '[appTooltipManager]'
+  selector: '[tooltipManager][ngbTooltip]'
 })
 export class TooltipManagerDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef, private tooltip: NgbTooltip) {
+    console.log(element);
+    console.log(tooltip);
+  }
 
 }
