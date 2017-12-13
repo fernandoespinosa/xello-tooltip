182326a160221d205f973281df16e23db0e50b08
fernandoespinosa@fernandoespinosa.org <fernandoespinosa@fernandoespinosa.org>
2017-12-12 23:19:02 -0800

    [mod] tooltip-manager.directive: Got it!
    	- check click is outside _windowRef member from NgbTooltip

--- src/app/directives/tooltip-manager.directive.ts
+++ src/app/directives/tooltip-manager.directive.ts
@@ -1,28 +1,33 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, ComponentRef } from '@angular/core';
 import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 
 @Directive({
   selector: '[tooltipManager][ngbTooltip]'
 })
 export class TooltipManagerDirective {
+  private tooltipId: string;
 
-  private _tooltipId: string;
-
-  constructor(private _element: ElementRef, private _tooltip: NgbTooltip) {
-    this._tooltipId = (_tooltip as any)._ngbTooltipWindowId;
+  constructor(private element: ElementRef, private tooltip: NgbTooltip) {
+    this.tooltipId = (tooltip as any)._ngbTooltipWindowId;
   }
 
   @HostListener('document:click', ['$event'])
   private documentClick(event: MouseEvent): void {
-    console.log('documentClick @:', this._tooltipId);
+    console.log('documentClick @:', this.tooltipId);
     
-    if (this._tooltip && this._tooltip.isOpen()) {
-      console.log('tooltip is open:', this._tooltip);
+    if (this.tooltip && this.tooltip.isOpen()) {
+      console.log('tooltip is open:', this.tooltip);
 
-      if (this._element.nativeElement.contains(event.target)) {
-        console.log('clicked inside:', this._element);
+      if (this.element.nativeElement.contains(event.target)) {
+        console.log('clicked inside:', this.element);
       } else {
-
+        const tooltipWindowRef: ComponentRef<NgbTooltip> = (this.tooltip as any)._windowRef;
+        if (tooltipWindowRef.location.nativeElement.contains(event.target)) {
+          console.log('clicked inside:', tooltipWindowRef);
+        } else {
+          console.log('clicked outside:', tooltipWindowRef);
+          this.tooltip.close();
+        }
       }
     }
   }
