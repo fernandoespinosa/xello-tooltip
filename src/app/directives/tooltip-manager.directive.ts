ee4270e6b0b6494fb1177337dadacfc1d6e8c789
fernandoespinosa@fernandoespinosa.org <fernandoespinosa@fernandoespinosa.org>
2017-12-12 23:04:13 -0800

    [mod] tooltip-manager.directive: log state and position on document:click
    [mod] app.component: change to triggers="manual"

--- src/app/directives/tooltip-manager.directive.ts
+++ src/app/directives/tooltip-manager.directive.ts
@@ -1,14 +1,30 @@
-import { Directive, ElementRef } from '@angular/core';
-import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap/tooltip/tooltip';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 
 @Directive({
   selector: '[tooltipManager][ngbTooltip]'
 })
 export class TooltipManagerDirective {
 
-  constructor(private element: ElementRef, private tooltip: NgbTooltip) {
-    console.log(element);
-    console.log(tooltip);
+  private _tooltipId: string;
+
+  constructor(private _element: ElementRef, private _tooltip: NgbTooltip) {
+    this._tooltipId = (_tooltip as any)._ngbTooltipWindowId;
+  }
+
+  @HostListener('document:click', ['$event'])
+  private documentClick(event: MouseEvent): void {
+    console.log('documentClick @:', this._tooltipId);
+    
+    if (this._tooltip && this._tooltip.isOpen()) {
+      console.log('tooltip is open:', this._tooltip);
+
+      if (this._element.nativeElement.contains(event.target)) {
+        console.log('clicked inside:', this._element);
+      } else {
+
+      }
+    }
   }
 
 }
